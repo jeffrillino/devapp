512337fc3fbeb6f387fb07329bf6ec99a70f5780
jeffrillino <jeffry.amachree@gmail.com>
2020-09-17 19:57:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.get("/", (req, res) => {
  * Server Activation
  */
 app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening to HTTP requests on http://localhost:${port}`);
 });
